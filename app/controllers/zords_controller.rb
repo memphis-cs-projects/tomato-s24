64f3bd939e036aa0d2f707aeb3e21a4aaf84bd60
Vishal Reddy <c.vishal.reddy108@gmail.com>
2024-04-13 21:18:34 +0530

    Made changes to added new zord action to index page

--- app/controllers/zords_controller.rb
+++ app/controllers/zords_controller.rb
@@ -1,5 +1,5 @@
 class ZordsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :require_vendor, only: [:new, :create]
 
   def index
